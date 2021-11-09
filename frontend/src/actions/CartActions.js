e5aa0f56dcdee4ad97880547026a5151540c604f
Prem3081 <prem.kumar@stepswatch.com>
2021-11-09 10:19:27 +0530

    Storing cart Locally

--- frontend/src/actions/CartActions.js
+++ frontend/src/actions/CartActions.js
@@ -12,6 +12,58 @@ import {
   CART_LIST_FAIL,
 } from "../constants/cartConstants";
 
+const addItemInLocalCart = async (requestBody) => {
+  try {
+    const { _id, quantity } = requestBody.products[0];
+    let data = await getLocalCart();
+    let cart = data.cart;
+    data = await APICore("/product/single", "post", null, {
+      productId: _id,
+    });
+    const product = data.product;
+    let added = false;
+    for (let item of cart) {
+      if (item._id === _id) {
+        item.quantity = quantity;
+        added = true;
+      }
+    }
+    if (!added) {
+      cart.push({
+        _id,
+        quantity,
+        name: product.name,
+        description: product.description,
+        image: product.images[0],
+        amount: product.amount,
+      });
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+  } catch (error) {
+    console.log(error);
+  }
+};
+const removeItemFromLocalCart = async (requestBody) => {
+  const { product } = requestBody;
+  let data = await getLocalCart();
+  let cart = data.cart;
+  let updatedCart = [];
+  for (let item of cart) {
+    if (item._id !== product) {
+      updatedCart.push(item);
+    }
+  }
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
+};
+
+const getLocalCart = async () => {
+  let cart = localStorage.getItem("cart");
+
+  if (!cart) localStorage.setItem("cart", JSON.stringify([]));
+  cart = localStorage.getItem("cart");
+  return { cart: JSON.parse(cart) };
+};
+
 export const getCartItems = () => async (dispatch, getState) => {
   try {
     const {
@@ -21,8 +73,11 @@ export const getCartItems = () => async (dispatch, getState) => {
       type: CART_LIST_REQUEST,
     });
 
-    const data = await APICore("/getCartItems", "get", token);
-
+    let data;
+    if (token) data = await APICore("/getCartItems", "get", token);
+    else {
+      data = await getLocalCart();
+    }
     dispatch({
       type: CART_LIST_SUCCESS,
       payload: data.cart,
@@ -44,18 +99,35 @@ export const addToCart = (requestBody) => async (dispatch, getState) => {
       type: ADD_TO_CART_REQUEST,
     });
 
-    const data = await APICore("/addToCart", "post", token, requestBody);
+    let data;
+    if (token) {
+      data = await APICore("/addToCart", "post", token, requestBody);
 
-    dispatch({
-      type: ADD_TO_CART_SUCCESS,
-      payload: data,
-    });
-    const cartdata = await APICore("/getCartItems", "get", token);
+      dispatch({
+        type: ADD_TO_CART_SUCCESS,
+        payload: data,
+      });
+      const cartdata = await APICore("/getCartItems", "get", token);
 
-    dispatch({
-      type: CART_LIST_SUCCESS,
-      payload: cartdata.cart,
-    });
+      dispatch({
+        type: CART_LIST_SUCCESS,
+        payload: cartdata.cart,
+      });
+    } else {
+      console.log(requestBody);
+      data = await addItemInLocalCart(requestBody);
+
+      dispatch({
+        type: ADD_TO_CART_SUCCESS,
+        payload: data,
+      });
+      const cartdata = await getLocalCart();
+
+      dispatch({
+        type: CART_LIST_SUCCESS,
+        payload: cartdata.cart,
+      });
+    }
   } catch (e) {
     dispatch({
       type: ADD_TO_CART_FAIL,
@@ -73,18 +145,34 @@ export const removeFromCart = (requestBody) => async (dispatch, getState) => {
       type: REMOVE_FROM_CART_REQUEST,
     });
 
-    const data = await APICore("/removeFromCart", "post", token, requestBody);
+    let data;
+    if (token) {
+      data = await APICore("/removeFromCart", "post", token, requestBody);
 
-    dispatch({
-      type: REMOVE_FROM_CART_SUCCESS,
-      payload: data,
-    });
-    const cartdata = await APICore("/getCartItems", "get", token);
+      dispatch({
+        type: REMOVE_FROM_CART_SUCCESS,
+        payload: data,
+      });
+      const cartdata = await APICore("/getCartItems", "get", token);
 
-    dispatch({
-      type: CART_LIST_SUCCESS,
-      payload: cartdata.cart,
-    });
+      dispatch({
+        type: CART_LIST_SUCCESS,
+        payload: cartdata.cart,
+      });
+    } else {
+      data = await removeItemFromLocalCart(requestBody);
+
+      dispatch({
+        type: REMOVE_FROM_CART_SUCCESS,
+        payload: data,
+      });
+      const cartdata = await getLocalCart();
+
+      dispatch({
+        type: CART_LIST_SUCCESS,
+        payload: cartdata.cart,
+      });
+    }
   } catch (e) {
     dispatch({
       type: REMOVE_FROM_CART_FAIL,
--- frontend/src/actions/UserActions.js
+++ frontend/src/actions/UserActions.js
@@ -9,6 +9,11 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
+import {
+  ADD_TO_CART_REQUEST,
+  ADD_TO_CART_SUCCESS,
+  CART_LIST_SUCCESS,
+} from "../constants/cartConstants";
 
 export const login = (requestBody) => async (dispatch) => {
   try {
@@ -18,7 +23,27 @@ export const login = (requestBody) => async (dispatch) => {
 
     const data = await APICore("/user/login", "post", null, requestBody);
     localStorage.setItem("essentialUser", data.token);
+    let cart = localStorage.getItem("cart");
+    if (cart) {
+      cart = JSON.parse(cart);
+      dispatch({
+        type: ADD_TO_CART_REQUEST,
+      });
+      const addToCartdata = await APICore("/addToCart", "post", data.token, {
+        products: cart,
+      });
 
+      dispatch({
+        type: ADD_TO_CART_SUCCESS,
+        payload: data,
+      });
+      const cartdata = await APICore("/getCartItems", "get", data.token);
+
+      dispatch({
+        type: CART_LIST_SUCCESS,
+        payload: cartdata.cart,
+      });
+    }
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data.token,
@@ -59,6 +84,7 @@ export const register = (requestBody) => async (dispatch) => {
 export const logout = () => async (dispatch) => {
   try {
     localStorage.removeItem("essentialUser");
+    localStorage.removeItem("cart");
     dispatch({
       type: USER_LOGOUT,
       payload: null,
